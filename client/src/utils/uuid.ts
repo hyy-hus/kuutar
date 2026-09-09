862da791d50ca9c6027d8f15e988cdbff698ea3e
Janne Saranpää <janne@jssa.fi>
2026-09-09 13:09:30 +0300

    chore(api): update openapi schema and add uuid null guard

--- client/src/utils/uuid.ts
+++ client/src/utils/uuid.ts
@@ -1,3 +1,4 @@
-export function readable_uuid(uuid: string): string {
-    return uuid.slice(-8);
+export function readable_uuid(id?: string | null): string {
+    if (!id) return ''
+    return id.slice(0, 8)
 }
